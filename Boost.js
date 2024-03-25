c7ab7e24fbfb29915d32bfc5a008e202e2bdb912
AlexandroRobert <162671978+AlexandroRobert@users.noreply.github.com>
2024-03-25 19:55:32 +0700

    Add files via upload

--- Boost.js
+++ Boost.js
@@ -42,3 +42,48 @@ export function SetInterval(Tick, Callback) {
   Timer.RestartLoop(Tick)
   return TIMER
 }
+
+// Создаёт новую зону с триггером и цветом
+// @param {Object} Options - опции создания зоны
+//    Все нужные параметры лежат в <see href="MAIN_OPTIONS">
+// @return {Object} - зона, состовляющая из 2 частей: триггер, сервис отображения
+export function CreateArea(Options) {
+  const MAIN_OPTIONS = {
+    // Состовляющая отображение
+    view: {
+      Enable: true, 
+      Color: new BASIC.Color(0, 0, 0, 0)
+    },
+    // Состовляющая триггера
+    trigger: {
+      Enable: true, 
+      Tags: ['default'], 
+      Name: 'default', 
+      OnEnter: function (p, a) => {
+        p.Ui.Hint.Value = `вы вошли в зону ${a.Name}`
+      },
+      OnExit: function (p, a) => {
+        p.Ui.Hint.Value = `вы вышли из зоны ${a.Name}`
+      }
+    }
+  }
+  
+  const RESULT = Object.assign(Options, MAIN_OPTIONS)
+  
+  let trigger = API.AreaPlayerTriggerService.Get(RESULT.trigger.Name)
+  trigger.Tags = RESULT.trigger.Tags
+  trigger.Enable = RESULT.trigger.Enable
+  trigger.OnEnter.Add(RESULT.trigger.OnEnter)
+  trigger.OnExit.Add(RESULT.trigger.OnExit)
+  
+  let view = API.AreaViewService.Get(RESULT.trigger.Name)
+  view.Tags   = RESULT.trigger.Tags
+  view.Enable = RESULT.view.Enable
+  view.Color  = RESULT.view.Color
+  
+  return { trigger, view }
+}
+
+function generateId(str) {
+  return `${str.length * 2}_${str[Math.floor(Math.random() * str.length)]}`
+}
